5ef03956ecfb9268b6d80fb5c34eddc60e7331df
kittyfur <kittyfur@protonmail.com>
2023-05-17 21:42:14 -0700

    add 'moody' and 'low' random lock algorithms

--- random_lock.py
+++ random_lock.py
@@ -10,7 +10,7 @@ def main():
     parser.add_argument('max_minutes', default=None, type=int, metavar='N',
                         help='maximum minutes to add to the lock time. If not already locked, creates a new sessions')
     parser.add_argument('--style', default='uniform', help='choose a random number generator',
-                        choices=['uniform', 'middle', 'high'])
+                        choices=['uniform', 'moody', 'low', 'middle', 'high'])
 
     args = parser.parse_args()
 
@@ -20,17 +20,43 @@ def main():
         if args.max_minutes == 0:
             print(f"no time added")
             return
-        res = lock_max(args.max_minutes, args.style)
+        res = lock_up_to(args.max_minutes, args.style)
         vault_ctl.print_response(res)
     else:
         parser.print_help()
 
 
-def lock_max(max_minutes, style):
+def lock_up_to(max_minutes, style):
+    if style == 'moody':
+        style = style_based_on_mood()
+
     minutes = calculate_minutes(max_minutes, style)
     dt = vault_ctl.add_minutes_to_now(minutes)
     print(f"adding {minutes} minutes")
     return vault_ctl.set_unlock_time(dt)
+
+
+def style_based_on_mood():
+    # determine mood on a scale of zero to four
+    mood = random.randint(0, 4)
+    # convert mood from a number to an emotion
+    if mood < 1:
+        emotion = 'hesitant'
+        style = 'low'
+    elif mood < 2:
+        emotion = 'fair-minded'
+        style = 'uniform'
+    elif mood < 3:
+        emotion = 'ambivalent'
+        style = 'middle'
+    else:
+        emotion = 'enthusiastic'
+        style = 'high'
+
+    # print emotion in english
+    print(f"The AI is feeling {emotion} about locking you up.")
+
+    return style
 
 
 def calculate_minutes(max_minutes, style) -> int:
@@ -40,6 +66,8 @@ def calculate_minutes(max_minutes, style) -> int:
         return int(gauss(max_minutes))
     elif style == 'high':
         return int(random.triangular(1, max_minutes, max_minutes))
+    elif style == 'low':
+        return int(random.triangular(1, max_minutes, 1))
 
 
 def gauss(maximum: int):
